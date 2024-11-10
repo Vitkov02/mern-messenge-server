9b3d3e625201cd1b2c61d67cd3eaef97d8da3af9
unknown <vitkowova@gmail.com>
2024-11-10 21:17:15 +0200

    feat(message.model) add message schema

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -72,7 +72,7 @@ export const login = async (req, res) => {
     }
 }
 
-export const logout = async (req, res) => {
+export const logout = (req, res) => {
     try {
         res.cookie('jwt', '', {maxAge: 0})
         res.status(200).json({message: 'Log out successfully'})
