bf4d32d8123f7d3bf2d84d646b6c1e6d21cb1c68
unknown <vitkowova@gmail.com>
2024-11-12 17:57:48 +0200

    feat add user controller and user router

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+
 import connectToMongoDB from './db.js'
+
 import authRouter from './routes/authRouter.js'
 import messageRoutes from './routes/messageRouter.js'
+import userRoutes from './routes/userRouter.js'
+
 import cors from 'cors'
 
 const app = express()
@@ -17,6 +21,7 @@ app.use(cookieParser())
 
 app.use('/api/auth', authRouter)
 app.use('/api/messages', messageRoutes)
+app.use('/api/users', userRoutes)
 
 const start = async() => {
     try {
